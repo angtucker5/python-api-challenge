f32db7d2b66c288dd7d1612ded9f85b189b5e0ce
Ang Tuckere <angtucker5@gmail.com>
2021-06-09 20:30:30 -0500

    update api key

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e1bfbc3353ff509dc926ae1bfaf041e5"
+weather_api_key = "Enter API Key"
 
 # Google API Key
-g_key = "AIzaSyAml0Vfxc12zOhbHYDPOtoM1PCLnFAL6fo"
+g_key = "Enter API Key"
